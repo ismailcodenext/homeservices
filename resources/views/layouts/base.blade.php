d7eb36739c6964f3687298febed042c33fa1be79
codenextitD <engr.ismail348@gmail.com>
2022-09-27 23:34:45 +0600

    authentication setup

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -13,7 +13,7 @@
     <link href="{{ asset('assets/css/theme-responsive.css') }}" rel="stylesheet" media="screen">
     <link href="{{ asset('assets/css/dtb/jquery.dataTables.min.css') }}" rel="stylesheet" media="screen">
     <link href="{{ asset('assets/css/select2.min.css') }}" rel="stylesheet" media="screen">
-    <link href="{{ asset('assets/css/toastr.min.css') }}" rel="stylesheet" media="screen">        
+    <link href="{{ asset('assets/css/toastr.min.css') }}" rel="stylesheet" media="screen">
     <script type="text/javascript" src="{{ asset('assets/js/jquery.js') }}"></script>
     <script type="text/javascript" src="{{ asset('assets/js/jquery-ui.1.10.4.min.js') }}"></script>
     <script type="text/javascript" src="{{ asset('assets/js/toastr.min.js') }}"></script>
@@ -120,8 +120,40 @@
                             <li><a href="servicesbycategory/18.html">Home Automation</a></li>
                         </ul>
                     </li>
-                    <li class="login-form"> <a href="index.php/register.html" title="Register">Register</a></li>
-                    <li class="login-form"> <a href="login.html" title="Login">Login</a></li>
+                    @if(Route::has('login'))
+                        @auth()
+                        @if(Auth::user()->utype=='ADM')
+                                <li class="login-form"> <a href="#" title="Register">My Account (Admin)</a>
+                                    <ul class="drop-down one-column hover-fade">
+                                        <li><a href="#">Dashboard</a></li>
+                                        <li><a href="{{route('logout')}}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();" >Logout</a></li>
+                                    </ul>
+                                </li>
+                            @elseif(Auth::user()->utype==='SVP')
+                                <li class="login-form"> <a href="#" title="Register">My Account (Service Provider)</a>
+                                    <ul class="drop-down one-column hover-fade">
+                                        <li><a href="#">Dashboard</a></li>
+                                        <li><a href="{{route('logout')}}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();" >Logout</a></li>
+                                    </ul>
+                                </li>
+                            @else
+                                <li class="login-form"> <a href="#" title="Register">My Account (Customer)</a>
+                                    <ul class="drop-down one-column hover-fade">
+                                        <li><a href="#">Dashboard</a></li>
+                                        <li><a href="{{route('logout')}}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();" >Logout</a></li>
+                                    </ul>
+                                </li>
+                            @endif
+                                <form id="logout-form" method="POST" action="{{route('logout')}}">
+                                    @csrf
+                                </form>
+                        @else
+                            <li class="login-form"> <a href="{{route('register')}}" title="Register">Register</a></li>
+                            <li class="login-form"> <a href="{{route('login')}}" title="Login">Login</a></li>
+                            @endif
+
+                    @endif
+
                     <li class="search-bar">
                     </li>
                 </ul>
@@ -231,8 +263,8 @@
                             <p class="text-xs-center crtext">&copy; 2021 SurfsideMedia. All Rights Reserved.</p>
                         </div>
                     </div>
-                </div>                
-            </div>            
+                </div>
+            </div>
         </footer>
     </div>
     <script type="text/javascript" src="{{asset('assets/js/nav/jquery.sticky.js') }}"></script>
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,12 @@ use App\Http\Livewire\HomeComponent;
 // });
 
 Route::get('/',HomeComponent::class)->name('home');
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
